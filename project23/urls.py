eb56d9ea5d945aa6e514adcfd98eed78295baf8f
pujitha2a <pujithaperam72@gmail.com>
2023-12-15 20:25:45 +0530

    first commit

--- project23/urls.py
+++ project23/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('display_topics/',display_topics,name='display_topics'),
+    path('display_webpage/',display_webpage,name='display_webpage'),
+    path('display_accessrecords/',display_accessrecords,name='display_accessrecords'),
+    path('insert_topic/',insert_topic,name='insert_topic'),
+    path('insert_webpage/',insert_webpage,name='insert_webpage'),
+    path('insert_access/',insert_access,name='insert_access'),
 ]
